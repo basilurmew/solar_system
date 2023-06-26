06c32c5a822503dbaf6b83637f862ab2db3af391
basilurmew <andrey.staroselets980@gmail.com>
2023-06-27 00:28:48 +0300

    тестовый запуск для понимания работы Git

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def welcome(name):
+
+    print(f'welcome to solar system club, {name}, we are going, to create a 3d model of solar system')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    welcome('Billy')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
